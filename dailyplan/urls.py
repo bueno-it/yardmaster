30d2a38123cc86b64a53abc8efeedf777138412e
Maurilio Bueno <mbueno@bueno-it.com>
2026-07-20 19:17:28 +0100

    Add new API endpoint for import_picking_v2

--- dailyplan/urls.py
+++ dailyplan/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('api/update-flag/',    views.update_flag,    name='dp_update_flag'),
     path('api/import-plan/',  views.import_plan,    name='dp_import_plan'),
     path('api/import-picking/', views.import_picking, name='dp_import_picking'),
+    path('api/import-picking2/', views.import_picking_v2, name='dp_import_picking_v2'),
     path('api/add-row/',      views.add_row,        name='dp_add_row'),
     path('api/delete-row/',   views.delete_row,     name='dp_delete_row'),
     path('export/',           views.export_plan,    name='dp_export'),
